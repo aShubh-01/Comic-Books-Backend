648416e2fc1dae3d9468fde6b430d6b1f11d314a
Ashubh01 <shubhamdhokare01@gmail.com>
2024-10-16 23:27:35 +0530

    Added Delete, Book details and Book list endpoints

--- controllers.js
+++ controllers.js
@@ -1,7 +1,7 @@
 const { prisma } = require('./config.js')
 const { createBookDataSchema, updateBookDataSchema } = require('./validationSchemas.js')
 
-const createBook = async (req, res) => {                   // Create comic book API
+const createBook = async (req, res) => {                   // Create comic book Endpoint
 
     const parsingResponse = createBookDataSchema.safeParse(req.body)   // Validate the data types of the fields
 
@@ -37,12 +37,12 @@ const createBook = async (req, res) => {                   // Create comic book 
         console.log('Unable to create comic book ', err)
     }
 
-    return res.status(500).json({                           // respond with an error message if database operation fails
+    return res.status(500).json({                            // Return message if endpoint fails to create comic book
         message: "Unable to create book"
     })
 }
 
-const updateBook = async (req, res) => {                         // Update comic book API
+const updateBook = async (req, res) => {                   // Update comic book Endpoint
 
     const comidBookId = parseInt(req.params.id);
 
@@ -77,29 +77,112 @@ const updateBook = async (req, res) => {                         // Update comic
         })
 
     } catch (err) {                                         // catch any error that might arise during update operation
-        console.log('Unable to create comic book ', err)
+        console.log('Unable to update comic book ', err)
     }
 
-    return res.json({
+    return res.status(500).json({                       // Return message if endpoint fails to update comic book
         message: "Unable to update comic book data"
     })
 }
 
-const deleteBook = (req, res) => {
-    res.json({
-        message: "Dek"
+const deleteBook = async (req, res) => {                   // Delete comic book Endpoint
+
+    const comidBookId = parseInt(req.params.id);
+
+    try {                                                
+        await prisma.comicBook.delete({      // If payload validation succeeds, delete the records from database
+            where: { id: comidBookId }
+        })
+
+        return res.status(200).json({                       // if the deletion succeeds, return an message
+            message: `Comic Book Deleted!`
+        })
+
+    } catch (err) {                                         // catch any error that might arise during delete operation
+        console.log('Unable to delete comic book', err)
+    }
+
+    res.json({                                           // Return message if endpoint fails to delete comic book
+        message: "Unable to delete comic book"
     })
 }
 
-const getBook = (req, res) => {
-    res.json({
-        message: "get"
+const getBook = async (req, res) => {                            // Get details of an book Endpoint
+
+    const comicBookId = parseInt(req.params.id);
+
+    try {                                                   // Get the details of the book using comic book id
+        const bookDetails = await prisma.comicBook.findFirst({
+            where: { id: comicBookId },
+            select: {
+                authorName: true,
+                bookName: true,
+                description: true,
+                publishedYear: true,
+                price: true,
+                discount: true,
+                pages: true,
+                isUsed: true
+            }
+        })
+
+        return res.status(200).json({
+            message: 'Book Details Fetched!',
+            bookDetails
+        })
+    } catch (err) {                                         // catch any error that might occur during find operation
+        console.log('Unable to get comic book data', err)
+    }
+
+    res.status(500).json({                           // Return message if endpoint fails to get comic book details
+        message: "Unable to get comic book data"
     })
 }
 
-const getBooksList = (req, res) => {
-    res.json({
-        message: "list"
+const getBooksList = async (req, res) => {
+    const { author, year : minYear, price : maxPrice, pages : maxPages, discount : minDiscount, isUsed : isBookUsed, sortBy,  page, limit }= req.query; // get query parameters
+    
+    let filters = [                                                     // build an filter array for 
+        author ? { authorName: author } : {},
+        minYear ? { publishedYear: { gte: parseInt(minYear) } } : {},
+        maxPrice ? { price: { lte: parseInt(maxPrice) } } : {},
+        maxPages ? { pages: { lte: parseInt(maxPages) } } : {},
+        minDiscount ? { discount: { gte: parseInt(minDiscount) } } : {},
+        isBookUsed ? { isUsed: isBookUsed } : {}
+    ]
+
+    const skip = (parseInt(page) - 1) * limit;
+
+    try {
+        let booksList = await prisma.comicBook.findMany({
+            where : filters.length > 0 ? { AND : filters } : {},
+            skip: skip,
+            take: parseInt(limit)
+        })
+
+        switch (sortBy) {
+            case 'price': booksList = booksList.sort((a, b) => b.price - a.price)
+            break;
+            case 'discount': booksList = booksList.sort((a, b) => b.discount - a.discount)
+            break;
+            case 'year': booksList = booksList.sort((a, b) => b.year - a.year)
+            break;
+            case 'pages': booksList = booksList.sort((a, b) => b.pages - a.pages)
+            break;
+            case 'alphabets': booksList = booksList.sort((a, b) => a.bookName.localeCompare(b.bookName))
+            break;
+        }
+
+        return res.status(200).json({
+            message: "Books fetched!",
+            booksList
+        })
+    } catch (err) {
+        console.log('Unable to fetch books list', err)
+    }
+    
+    return res.status(500).json({
+        message: "Unable to fetch books list"
     })
 }
 
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { createBook, updateBook, deleteBook, getBook, getBooksList } = require('.
 const router = express()      // create main router
 
 router.use(express.json())           // makes sure express is able to parse the json data
-router.use(cors())                   // allows request from cross origins site
+router.use(cors())                   // allows request from cross origins sites
 
-router.post('/create', createBook)          // API to create a book
-router.put('/update/:id', updateBook)       // API to update a book
-router.delete('/delete/:id', deleteBook)    // API to delete a book
-router.get('/details/:id', getBook)         // API to get book details
-router.post('/list', getBooksList)      // API to get an list of books
+router.post('/create', createBook)          // Endpoint to create an book
+router.put('/update/:id', updateBook)       // Endpoint to update an book
+router.delete('/delete/:id', deleteBook)    // Endpoint to delete an book
+router.get('/details/:id', getBook)         // Endpoint to get book details
+router.get('/list', getBooksList)           // Endpoint to get an list of available books
 
 router.listen(port, () => {                         // listen on given port
     console.log('Backend running on port ', port)
